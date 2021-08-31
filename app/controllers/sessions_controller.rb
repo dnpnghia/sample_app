55d290bed9226a8213ef4c703573c3825829b9ac
dnpnghia <idnpnlol@gmail.com>
2021-08-31 13:21:56 +0700

    chapter 9: advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate(params[:session][:password])
+    if user.try(:authenticate, params[:session][:password])
       flash[:success] = t "sessions.login_success"
-      log_in user
+      checkbox_remember? user
       redirect_to user_path id: user.id
     else
       flash.now[:danger] = t "sessions.login_failed"
